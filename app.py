4edc0dfd458bc279c3bcf3e8597734e76da3cff6
Lewis Shadrake <lewisshadrake.work@gmail.com>
2024-06-19 15:11:29 +0100

    multiple pulls and WIP transistion

--- app.py
+++ app.py
@@ -10,16 +10,22 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/pull", methods=["POST"])
+@app.route("/pull", methods=["GET", "POST"])
 def pull():
+    if request.method == "GET":
+        return redirect("/")
     if not request.form.get("bannerID"):
         return redirect("/")
     if not request.form.get("pullNum"):
         return redirect("/")
     
+    units = []
     with sqlite3.connect("lyres.db") as con:
         cur = con.cursor()
-        units = cur.execute("SELECT id, rarity FROM lyres WHERE id IN (SELECT lyres_id FROM banner_units WHERE banner_id = ?)", request.form.get("bannerID"))
-        unit = random.choice(units.fetchall())
+        pool = cur.execute("SELECT id, rarity FROM lyres WHERE id IN (SELECT lyres_id FROM banner_units WHERE banner_id = ?)", request.form.get("bannerID")).fetchall()
+    for i in range(int(request.form.get("pullNum"))):
+        units.append(random.choice(pool))
 
-    return render_template("pull.html", unit=unit)
+
+
+    return render_template("pull.html", units=units)
