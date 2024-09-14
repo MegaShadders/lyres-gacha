8fa760a50d85fcf8b273e85bee8a9f85688119a5
Lewis Shadrake <lewisshadrake.work@gmail.com>
2024-09-14 16:55:54 +0100

    insert new users into db, change placeholder user id into session id

--- app.py
+++ app.py
@@ -24,6 +24,15 @@ def callback():
     code = request.args['code']
     access_token = client.oauth.get_access_token(code, REDIRECT_URI).access_token
     session['token'] = access_token
+
+    with sqlite3.connect("lyres.db") as con:
+        cur = con.cursor()
+        bearer_client = APIClient(session.get('token'), bearer=True)
+        current_user = bearer_client.users.get_current_user()
+        #If discord id does not exist insert new user in db
+        if cur.execute("SELECT EXISTS(SELECT id FROM users WHERE id = ?)", (current_user.id,)).fetchone()[0] == 0:
+            cur.execute("INSERT INTO users (id, username) VALUES(?, ?)", [current_user.id, current_user.username])
+        session['id'] = current_user.id
     return redirect("/")
 
 
@@ -49,7 +58,7 @@ def pull():
         poolSSR = cur.execute("SELECT id, rarity FROM units WHERE id IN (SELECT unit_id FROM banner_units WHERE banner_id = ?) AND rarity = 'SSR'", request.form.get("bannerID")).fetchall()
         poolSR = cur.execute("SELECT id, rarity FROM units WHERE id IN (SELECT unit_id FROM banner_units WHERE banner_id = ?) AND rarity = 'SR'", request.form.get("bannerID")).fetchall()
         poolR = cur.execute("SELECT id, rarity FROM units WHERE id IN (SELECT unit_id FROM banner_units WHERE banner_id = ?) AND rarity = 'R'", request.form.get("bannerID")).fetchall()
-        pity = cur.execute("SELECT id, rarity, count, maximum FROM pity INNER JOIN user_pity ON pity.id = user_pity.pity_id WHERE id IN (SELECT pity_id FROM banner_pity WHERE banner_id = ?) AND user_id = ?", [request.form.get("bannerID"), 3]).fetchall()
+        pity = cur.execute("SELECT id, rarity, count, maximum FROM pity INNER JOIN user_pity ON pity.id = user_pity.pity_id WHERE id IN (SELECT pity_id FROM banner_pity WHERE banner_id = ?) AND user_id = ?", [request.form.get("bannerID"), session['id']]).fetchall()
         
         counts = [x[2] for x in pity] #Puts the counts for every pity in a list
         for i in range(int(request.form.get("pullNum"))): #For every pull
@@ -86,7 +95,7 @@ def pull():
                         counts[j] = 0
         # Update database with new pity counts
         for k in range(len(pity)):
-            cur.execute("UPDATE user_pity SET count = ? WHERE pity_id = ? AND user_id = ?", [counts[k], pity[k][0], 3])   
+            cur.execute("UPDATE user_pity SET count = ? WHERE pity_id = ? AND user_id = ?", [counts[k], pity[k][0], session['id']])   
 
     return render_template("pull.html", units=units, pullNum=request.form.get("pullNum"), bannerID=request.form.get("bannerID"))
 
@@ -99,7 +108,7 @@ def collection():
     with sqlite3.connect("lyres.db") as con:
         cur = con.cursor()
         units = cur.execute("SELECT id, rarity FROM units ORDER BY LENGTH(rarity) DESC").fetchall()
-        collectedUnits = cur.execute("SELECT unit_id, copies FROM collections WHERE user_id = 3").fetchall()
+        collectedUnits = cur.execute("SELECT unit_id, copies FROM collections WHERE user_id = ?", (session['id'],)).fetchall()
 
         unitList = [x[0] for x in units]
         collectedList = [x[0] for x in collectedUnits]
@@ -108,6 +117,6 @@ def collection():
             if unitList[i] in collectedList:
                 units[i] = units[i] + (True, )
             else:
-                units[i] = units[i] + (True, ) # Set to False when not testing
+                units[i] = units[i] + (False, ) # Set to False when not testing
     
     return render_template("collection.html", units=units, collectedUnits=collectedUnits)
