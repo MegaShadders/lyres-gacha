8e15c9977bcfb36a156682d11052e30070dd1908
Lewis Shadrake <lewisshadrake.work@gmail.com>
2025-06-04 13:17:04 +0100

    change load_user_currency to return list of currency dicts

--- user.py
+++ user.py
@@ -7,16 +7,17 @@ import sqlite_helper
 
 def load_user_currency(user_id):
     with sqlite3.connect("lyres.db") as con:
+        con.row_factory = sqlite_helper.dict_factory
         cur = con.cursor()
-        currencies = cur.execute("SELECT amount FROM user_currency WHERE user_id = ?", [user_id]).fetchall()
+        currencies = cur.execute("SELECT currency_id, amount FROM user_currency WHERE user_id = ?", [user_id]).fetchall()
     return currencies
 
 def load_user():
     bearer_client = APIClient(session.get('token'), bearer=True)
     current_user = bearer_client.users.get_current_user()
-    session_currencies = load_user_currency(current_user.id)
+    currencies = load_user_currency(current_user.id)
 
-    return current_user, session_currencies
+    return current_user, currencies
 
 def load_user_missions(user_id):
     load_user_daily(user_id)
