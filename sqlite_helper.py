79e1eb25b2b5f48d8d3d17299e8005dc455785ec
Lewis Shadrake <lewisshadrake.work@gmail.com>
2025-06-04 13:09:20 +0100

    claim mission currency change moved to helper function

--- sqlite_helper.py
+++ sqlite_helper.py
@@ -14,4 +14,5 @@ def claim_mission(user_id, mission):
     with sqlite3.connect("lyres.db") as con:
         cur = con.cursor()
         cur.execute("UPDATE user_missions SET claimable = 0 WHERE user_id = ? AND missions_id = ?", [user_id, mission["id"]])
-        cur.execute("UPDATE user_currency SET amount = amount + ? WHERE user_id = ? AND currency_id = ?", [mission["reward"], user_id, mission["currency_id"]])
+        change_currency([mission["reward"], user_id, mission["currency_id"]])
+
